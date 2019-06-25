6a388491d897fd5f0ec4f80ee64f73c927b42397
sithsiri <sith@live.com>
2019-06-25 16:21:11 -0500

    Updated to use requests
    because requests is so much better. Just look at all the lines we got rid of! and all our functionality remains perfectly intact. Beautiful.

--- gsearch.py
+++ gsearch.py
@@ -1,9 +1,5 @@
-#import requests
+import requests
 import bs4
-import pycurl
-import io
-import certifi
-import json
 
 class Error(Exception):
 	pass
@@ -17,19 +13,11 @@ class DecodeError(Error):
 def reverse_gimage(imagelink : str):
 	try:
 		redirURL = "https://images.google.com/searchbyimage?image_url=" + imagelink
-		final = io.BytesIO()
-		curling = pycurl.Curl()
-		curling.setopt(curling.CAINFO, certifi.where())
-		curling.setopt(curling.URL, redirURL)
-		curling.setopt(curling.FOLLOWLOCATION, 1)
-		curling.setopt(curling.USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
-		curling.setopt(curling.WRITEFUNCTION, final.write)
-		curling.perform()
-		curling.close()
+		final = requests.get(redirURL, headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"})
 	except:
 		raise NetworkError("couldn't get webpage from Google")
 	try:
-		soup = bs4.BeautifulSoup(final.getvalue().decode('UTF-8'), 'html.parser')
+		soup = bs4.BeautifulSoup(final.text, 'html.parser')
 		rawoutput = soup.findAll('a', attrs={'class':'fKDtNb'})
 		output = []
 	except:
@@ -48,18 +36,10 @@ def reverse_gimage_link(imagelink : str):
 def get_autocomplete_entry(query : str):
 	try:
 		url = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + query.replace('+', '%2B').replace(' ', '+')
-		final = io.BytesIO()
-		curling = pycurl.Curl()
-		curling.setopt(curling.CAINFO, certifi.where())
-		curling.setopt(curling.URL, url)
-		curling.setopt(curling.FOLLOWLOCATION, 1)
-		curling.setopt(curling.USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
-		curling.setopt(curling.WRITEFUNCTION, final.write)
-		curling.perform()
-		curling.close()
+		final = requests.get(url, headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"})
 	except:
 		raise NetworkError("couldn't get webpage from Google")
 	try:
-		return(json.loads(final.getvalue().decode('UTF-8'))[1])
+		return(final.json()[1])
 	except:
 		raise DecodeError("failed to decode properly. improper or unexpected data was likely supplied. updating this module may fix this")
